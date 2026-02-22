63e617180c4bc5f75e73d084a822f18a24578a4b
sumon2003 <sumonkhanbd2003@gmail.com>
2026-02-22 09:53:40 +0600

    Refactor code structure for improved readability and maintainability

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -5,7 +5,7 @@ export const projectsData = [
     description: "A fully responsive online store built with React and Tailwind CSS.",
     tags: ["React", "Tailwind CSS", "Responsive Design"],
     link: "https://github.com/sumon2003/Noor-Fragrance-Shop",
-    image: "src/assets/img/project2.png" 
+    image: "/img/project2.png" 
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const projectsData = [
     description: "A health monitoring frontend project for elderly people, built with Pure HTML, CSS.",
     tags: ["JavaScript", "HTML", "CSS"],
     link: "https://sumon2003.github.io/Elder-Care-Front-end-Project/",
-    image: "src/assets/img/project1.png"
+    image: "/img/project1.png"
   },
   {
     id: 3,
@@ -21,6 +21,6 @@ export const projectsData = [
     description: "The very project you are looking at right now!",
     tags: ["React", "Glassmorphism", "Framer Motion"],
     link: "#",
-    image: "src/assets/img/project3.jpeg"
+    image: "/img/project3.jpeg"
   }
 ];
